be5e9b26e6e909d9d7136a1be83f791c7cdd5b53
Xiong Zhou <xzhou@redhat.com>
2015-01-27 17:20:23 +0800

    init

---
+++ app/src/main/java/com/example/zx/myapplication/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.zx.myapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void buttonClicked(View view) {
+        TextView tv = (TextView)findViewById(R.id.textView);
+        tv.setText("ps:\n");
+
+        try {
+            Runtime r = Runtime.getRuntime();
+            Process p = r.exec("ps -ef");
+            p.waitFor();
+            BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = "";
+
+            while ((line = b.readLine()) != null) {
+                System.out.println(line);
+                tv.append(line);
+            }
+
+            b.close();
+        }
+        catch (IOException ie) {}
+        catch (InterruptedException inte) {}
+    }
+}
